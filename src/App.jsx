64e1d8d4fe1d4df10eac2d9b3b5524ebd15d973f
Shorif4321 <shorifali4321@gmail.com>
2025-09-20 23:23:36 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <div>
       <Header></Header>
       <Home></Home>
-      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>SHOP here</h1>
+        </div>
+    );
+};
+
+export default Shop;
